d89591a75669b71f112fec6b35628f856bd41d51
Jonatan Nevo <thejonatanmr@gmail.com>
2019-02-23 15:49:50 +0200

    Fixed find all scouting files, KNOWS ISSUES: Coping files does not work

--- file_sucker.py
+++ file_sucker.py
@@ -1,23 +1,29 @@
 from adb.client import Client as AdbClient
 import os
+import re
 
 DEFAULT_SERVER_IP = "127.0.0.1"
 DEFAULT_SERVER_PORT = 5037
 
-SCOUTING_FOLDER_OUTPUT_PATH = ""  # TODO: Find and fill.
-FIND_SCOUTING_FILES_COMMAND = "ls {path}".format(path=SCOUTING_FOLDER_OUTPUT_PATH)
+SCOUTING_FOLDER_OUTPUT_PATH = "/sdcard/documents"
+LIST_DOCUMENTS_FOLDER = "ls {path}".format(path=SCOUTING_FOLDER_OUTPUT_PATH)
+SCOUTING_FOLDERS_FORMAT = "Robot Scouter export_[0-9]*"
+SCOUTING_FILES_FORMAT = ".*.xlsx"  # TODO: need to change according to the name of the real files.
+LIST_SCOUTING_FILES = "ls {father_path}/{child_path}"
+SCOUTING_FILES_FULL_PATH_FORMAT = "{base_folder}/{export_folder}/{filename}"
 
 OUTPUT_FOLDER = "Scouting"
 FILE_ID = 0
-FILE_NAME = "x{id}.txt".format(id=FILE_ID)
+FILE_NAME = "x{id}.xlsx"
 
-REMOVE_FILE_COMMAND = "rm {file_path}"
+REMOVE_FILE_COMMAND = "rm -r {file_path}"
 
 
-class FileSucker(object):
+class FilePuller(object):
     """
-    Sucks scouting files from connected phone.
+    Pulls scouting files from connected phone.
     """
+
     def __init__(self):
         """
         Sets the adb client, TODO: Check if it opens the adb server automatically or not.
@@ -32,22 +38,39 @@ class FileSucker(object):
         :return: A list of all the found scouting files paths.
         :rtype: list
         """
-        paths = [self.__phone.shell(FIND_SCOUTING_FILES_COMMAND)]
-        #TODO: this wont work, I do not know how the scouting arranges the files, Needs to be fixed.
-        return [SCOUTING_FOLDER_OUTPUT_PATH + path for path in paths]
+        output_list = []
+        list_documents = self.__phone.shell(LIST_DOCUMENTS_FOLDER)
+        scouting_folders = [folder_path.replace(" ", r"\ ") for folder_path in
+                            re.findall(SCOUTING_FOLDERS_FORMAT, list_documents)]
 
-    def cut_file(self, file_path):
+        for folder_path in scouting_folders:
+            list_scouting_files = self.__phone.shell(LIST_SCOUTING_FILES.format(
+                                                        father_path=SCOUTING_FOLDER_OUTPUT_PATH,
+                                                        child_path=folder_path))
+
+            scouting_files = [file.replace(" ", r"\ ") for file in re.findall(SCOUTING_FILES_FORMAT, list_scouting_files)]
+
+            for file in scouting_files:
+                output_list.append(SCOUTING_FILES_FULL_PATH_FORMAT.format(
+                    base_folder=SCOUTING_FOLDER_OUTPUT_PATH,
+                    export_folder=folder_path,
+                    filename=file
+                ))
+
+        return output_list
+
+    def copy_file(self, file_path):
         """
-        Gets a file path and cuts it from the phone to the computer.
-        :param file_path: The path of the file to cut.
+        Gets a file path and copies it from the phone to the computer.
+        :param file_path: The path of the file to copy.
         """
         global FILE_ID
-        if not os.path.exists(OUTPUT_FOLDER) and os.path.isdir(OUTPUT_FOLDER):
+        if not os.path.exists(OUTPUT_FOLDER):
             os.mkdir(OUTPUT_FOLDER)
 
         FILE_ID += 1
-        self.__phone.pull(file_path, OUTPUT_FOLDER + FILE_NAME)
-        self.__phone.shell(REMOVE_FILE_COMMAND.format(file_path=file_path))
+        print self.__phone.shell("ls -la "+ file_path)
+        self.__phone.pull(file_path, OUTPUT_FOLDER+"/"+FILE_NAME.format(id=FILE_ID))
 
     def cut_all_scouting_files_from_phone(self):
         """
@@ -58,11 +81,11 @@ class FileSucker(object):
 
         for index, file_path in enumerate(scouting_files):
             print "Copying {current}/{total}".format(current=(index + 1), total=len(scouting_files))
-            self.cut_file(file_path)
+            self.copy_file(file_path)
 
 
 def main():
-    FileSucker().cut_all_scouting_files_from_phone()
+    FilePuller().cut_all_scouting_files_from_phone()
 
 
 if __name__ == '__main__':
